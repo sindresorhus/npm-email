1fb446fc7763eca938d1e4c367be6972a524342f
tbozhikov <t_bozhikov@yahoo.com>
2017-11-20 17:22:30 +0200

    Use WHATWG URL when calling Got (#3)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const {URL} = require('url');
 const got = require('got');
 const registryUrl = require('registry-url');
 
@@ -7,7 +8,8 @@ module.exports = username => {
 		return Promise.reject(new Error('username required'));
 	}
 
-	const url = `${registryUrl()}-/user/org.couchdb.user:${username}`;
+	const urlStr = `${registryUrl()}-/user/org.couchdb.user:${username}`;
+	const url = URL ? new URL(urlStr) : urlStr;
 
 	return got(url, {json: true})
 		.then(res => res.body.email)
--- test.js
+++ test.js
@@ -1,15 +1,24 @@
 import test from 'ava';
 import m from './';
 
-test('invalid input', t => {
-	t.throws(m(1), 'username required');
+test('invalid input', async t => {
+	await t.throws(m(1), 'username required');
 });
 
-test('unknown username', t => {
+test('unknown username', async t => {
 	const randomName = `asdasfgrgafadsgaf${Math.random().toString().slice(2)}`;
-	t.throws(m(randomName), `User ${randomName} doesn't exist`);
+	await t.throws(m(randomName), `User ${randomName} doesn't exist`);
 });
 
 test('valid username', async t => {
 	t.is(await m('sindresorhus'), 'sindresorhus@gmail.com');
 });
+
+// If URL class is present (NODE v7 and above), execute the las test
+const {URL} = require('url');
+
+if (URL) {
+	test('valid username with special char', async t => {
+		t.is(await m(`lukeramsden'`), 'lukeramsden8@gmail.com');
+	});
+}
