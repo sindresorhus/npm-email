4a38bd0f7250bdac88ca4950aba322553d63685b
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-09 21:25:02 +0100

    Add TypeScript definition, update dependencies

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Get the email of an npm user.
+ *
+ * @param username - npm username to look up.
+ * @returns A promise for the user's email address.
+ */
+export default function npmEmail(username: string): Promise<string>;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
+
 const {URL} = require('url');
 const got = require('got');
 const registryUrl = require('registry-url');
 
-module.exports = async username => {
+const npmEmail = async username => {
 	if (typeof username !== 'string') {
 		throw new TypeError('Username required');
 	}
@@ -13,11 +14,14 @@ module.exports = async username => {
 	try {
 		const {body} = await got(url, {json: true});
 		return body.email;
-	} catch (err) {
-		if (err && err.statusCode === 404) {
+	} catch (error) {
+		if (error && error.statusCode === 404) {
 			throw new Error(`User ${username} doesn't exist`);
 		}
 
-		throw err;
+		throw error;
 	}
 };
+
+module.exports = npmEmail;
+module.exports.default = npmEmail;
