798170a5ec2b754433aee03b8ca305152f0064a2
tommy-mitchell <tmitchell7@uh.edu>
2024-02-28 13:40:03 -0600

    improve 404 error handling

--- test.js
+++ test.js
@@ -1,11 +1,31 @@
 import test from 'ava';
+import {HTTPError} from 'ky';
 import npmEmail from './index.js';
 
-test('invalid input', async t => {
+test.serial('invalid input', async t => {
 	await t.throwsAsync(npmEmail(1), {message: 'Username required'});
 });
 
-test('valid username', async t => {
+test.serial('valid username', async t => {
 	t.is(await npmEmail('sindresorhus'), 'sindresorhus@gmail.com');
 	t.is(await npmEmail('vdemedes'), 'vadimdemedes@hey.com');
+});
+
+test.serial('non-existent user', async t => {
+	t.is(await npmEmail('nnnope'), undefined);
+});
+
+test.serial('handles 404', async t => {
+	globalThis.fetch = async () => {
+		throw new HTTPError(
+			new Response('', {status: 404, statusText: 'Not Found'}),
+		);
+	};
+
+	const {default: npmEmailMocked} = await import('./index.js');
+
+	await t.throwsAsync(
+		npmEmailMocked('nnnope'),
+		{message: 'User `nnnope` could not be found', code: 'ERR_NO_NPM_USER'},
+	);
 });
